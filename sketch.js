a79ae9386817ebd1fc723cdade22b3711ab05675
Learnerinprocess <104680298+Learnerinprocess@users.noreply.github.com>
2022-05-08 17:43:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+function preload(){
+  //pre-load images
+}
+
+function setup(){
+  createCanvas(400,400);
+  createSprite(track) = 100,200,300,150;
+  AddImage.path.png
+  createSprite(boy) = 10,20,30,40;
+  velocityY  = -3
+   addAnimation(running);
+   createSprite (boundary1) = 100,200,300,250
+   createSprite (boundary2) = 100,300,200,100
+   Sprite.visible = false
+}
+
+function draw() {
+  background(0);
+
+}
